656a5fb3ed752e4e925305ef097f6831d8986e17
RYANWILLIAMS <r007will@gmail.com>
2020-04-21 12:29:18 +0300

    Added user & crud

--- DBconnect.php
+++ DBconnect.php
@@ -1,7 +1,30 @@
 <?php
-
+define('DBServer','localhost');
+define('DBName', 'ics104811');
+define('username','root');
+define('password','');
 
 class DBconnect
 {
+    public $conn;
+
+    /**
+     * DBconnect constructor.
+     */
+    public function __construct()
+    {
+        $this->conn = mysqli_connect(
+            DBServer,
+            username,
+            password,
+            DBName)
+        or die(mysqli_error());
+
+    }
+
+    public function closeDB(){
+        mysqli_close($this->conn);
+    }
+
 
 }
---
+++ User.php
@@ -0,0 +1,41 @@
+<?php
+
+include "Crud.php";
+
+class User implements Crud{
+
+    public function save()
+    {
+        // TODO: Implement save() method.
+    }
+
+    public function readAll()
+    {
+        // TODO: Implement readAll() method.
+    }
+
+    public function readUnique()
+    {
+        // TODO: Implement readUnique() method.
+    }
+
+    public function search()
+    {
+        // TODO: Implement search() method.
+    }
+
+    public function update()
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function removeOne()
+    {
+        // TODO: Implement removeOne() method.
+    }
+
+    public function removeAll()
+    {
+        // TODO: Implement removeAll() method.
+    }
+}
